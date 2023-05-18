11984e18e0bc35b1509b8afef44d4095b043341d
Kevin Xu <kxu144@berkeley.edu>
2023-05-17 23:55:33 -0700

    reorg

---
+++ client/upload.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Handle the form submission
+    $("#upload-form").submit(function(event) {
+      event.preventDefault();
+  
+      // Get the selected image file
+      var file = $("#image-input")[0].files[0];
+  
+      
+    });
+  });
+  
